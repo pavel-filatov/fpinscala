98acb9c864f639cf7b25da7c3bddd50ecffb4431
Pavel Filatov <filatov.py@gmail.com>
2019-05-28 10:01:54 +0300

    started chapter 05 exercises

---
+++ src/main/scala/ch05laziness/Stream.scala
@@ -0,0 +1,54 @@
+package ch05laziness
+
+sealed trait Stream[+A] {
+  def headOption: Option[A] = this match {
+    case Empty => None
+    case Cons(a, as) => Some(a())
+  }
+
+  // ex. 5.1
+  def toList: List[A] = this match {
+    case Empty => Nil
+    case Cons(a, as) => a() :: as().toList
+  }
+
+  // ex. 5.2
+  def take(n: Int): Stream[A] = this match {
+    case Cons(a, as) if n > 0 => Cons(a, () => as().take(n - 1))
+    case _ => Empty
+  }
+
+  // ex. 5.3
+  def drop(n: Int): Stream[A] = this match {
+    case s if n == 0 => s
+    case Cons(a, as) if n > 0 => as().drop(n - 1)
+    case _ => Empty
+  }
+
+  // ex. 5.4
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(a, as) if p(a()) => Cons(a, () => as().takeWhile(p))
+    case _ => Empty
+  }
+
+  override def toString: String = this match {
+    case Empty => "Empty"
+    case Cons(a, as) => s"Stream(${a()}, ?)"
+  }
+}
+case object Empty extends Stream[Nothing]
+case class Cons[+A](hd: () => A, tl: () => Stream[A]) extends Stream[A]
+
+object Stream {
+  def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
+    lazy val head = () => hd
+    lazy val tail = () => tl
+    Cons(head, tail)
+  }
+
+  def empty[A]: Stream[A] = Empty
+
+  def apply[A](as: A*): Stream[A] =
+    if (as.isEmpty) empty
+    else cons(as.head, apply(as.tail: _*))
+}
---
+++ src/main/scala/ch05laziness/StreamsApp.scala
@@ -0,0 +1,23 @@
+package ch05laziness
+
+object StreamsApp extends App {
+
+  val a = Stream(1, 2, 3, 4, 5)
+  val b = Stream(10, 20, 30, 40, 50)
+
+  println("stream: " + a)
+  println("stream: " + b)
+  println("toList: " + a.toList)
+  println("toList: " + b.toList)
+
+  println()
+  println("take: " + a.take(2).toList)
+  println("take: " + b.take(30).toList)
+  println("drop: " + a.drop(3).toList)
+  println("drop: " + b.drop(30).toList)
+
+  println()
+  println("takeWhile:" + a.takeWhile(_ < 3).toList)
+  println("takeWhile:" + a.takeWhile(_ > 3).toList)
+
+}
